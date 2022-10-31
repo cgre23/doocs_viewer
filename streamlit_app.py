85de241c7c0e141b32366840f2262e77a039a63b
Christian Grech <chrisgre23@gmail.com>
2022-10-31 09:26:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,32 +7,32 @@ import pandas as pd
 gapminder_df = px.data.gapminder()
 
 st.set_page_config(
-    page_title="DOOCS Viewer3", page_icon="⬇", layout="centered"
+    page_title="DOOCS Viewer", page_icon="⬇", layout="centered"
 )
 
 st.title("DOOCS Viewer")
 """This demo demonstrates SASE2 orbit visualization using DOOCS HIST data"""
 
 
-#ßst.sidebar.title("Settings")
+st.sidebar.title("Settings")
 
 
     # Can be used wherever a "file-like" object is accepted:
     #st.write(dataframe)
 # If the user doesn't want to select which features to control, these will be used.
-#default_control_features = ["Young", "Smiling", "Male"]
+default_control_features = ["Young", "Smiling", "Male"]
 
 
-#if st.sidebar.checkbox("Show advanced options"):
+if st.sidebar.checkbox("Show advanced options"):
     # Let the user pick which features to control with sliders.
-#    control_features = st.sidebar.multiselect(
-#        "Exclude which cells?",
-#        ['Cell 1', 'Cell 2', 'Cell 3', 'Cell 4', 'Cell 5'],
-#        ['Cell 1'],
-#    )
-#else:
-#    # Don't let the user pick feature values to control.
-#    control_features = default_control_features
+    control_features = st.sidebar.multiselect(
+        "Exclude which cells?",
+        ['Cell 1', 'Cell 2', 'Cell 3', 'Cell 4', 'Cell 5'],
+        ['Cell 1'],
+    )
+else:
+    # Don't let the user pick feature values to control.
+    control_features = default_control_features
 
 # Insert user-controlled values from sliders into the feature vector.
     
@@ -40,16 +40,25 @@ st.title("DOOCS Viewer")
 
 
 
-#st.sidebar.title("Note")
-#st.sidebar.write(
-#    """The app is still in development.
-#     """
-#)
+st.sidebar.title("Note")
+st.sidebar.write(
+    """The app is still in development.
+     """
+)
+
+st.sidebar.caption("Developed by: Christian Grech (DESY, MXL)")
+st.sidebar.caption(f"Streamlit version `{st.__version__}`")
 
 
 
 dataframe = pd.read_pickle('doocs_orbit_20221021_viewer.pkl')
-
+minCell = dfm['Cell No'].min()
+maxCell = dfm['Cell No'].max()
+minValx = dfm['ValueX'].min()
+maxValx = dfm['ValueX'].max()
+minValy = dfm['ValueY'].min()
+maxValy = dfm['ValueY'].max()
+dfm['Time'] = dfm.index.strftime("%H:%M:%S.%f").str[:-5]
 
 # Animation year by year basis
 
@@ -69,5 +78,3 @@ animation = px.scatter(data_frame=gapminder_df,
 
 st.plotly_chart(animation, use_container_width=True)
 
-st.caption("Developed by: Christian Grech (DESY, MXL)")
-st.caption(f"Streamlit version `{st.__version__}`")
