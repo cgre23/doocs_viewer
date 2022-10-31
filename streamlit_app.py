dfd901042debaf51a85529a6c0d43185420e2958
Christian Grech <chrisgre23@gmail.com>
2022-10-31 11:36:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,8 +78,7 @@ animationX = px.line(data_frame=dfm,
           height=450)
 animationX.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = speed
 animationX.update_layout({
-    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-    'paper_bgcolor': 'rgba(0, 0, 0, 0)'},
+    'plot_bgcolor': 'rgba(0, 0, 0, 0)'},
     margin=dict(l=20, r=20, t=40, b=20)
 )
 animationX.update_yaxes( 
