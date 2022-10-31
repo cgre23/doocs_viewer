5940bc899dcbce33f0dad494cee9b553bf2f32ba
Christian Grech <chrisgre23@gmail.com>
2022-10-31 09:23:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import pandas as pd
 
 #import the data available in plotly.express
 gapminder_df = px.data.gapminder()
-dataframe = pd.read_pickle('doocs_BPMS_20221021_0619_0647.pkl')
 
 st.set_page_config(
     page_title="DOOCS Viewer3", page_icon="⬇", layout="centered"
@@ -15,42 +14,42 @@ st.title("DOOCS Viewer")
 """This demo demonstrates SASE2 orbit visualization using DOOCS HIST data"""
 
 
-st.sidebar.title("Settings")
+#ßst.sidebar.title("Settings")
 
-uploaded_file = st.sidebar.file_uploader("Choose a file", type=["pkl"])
-if uploaded_file is not None:
+
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_pickle(uploaded_file)
     #st.write(dataframe)
 # If the user doesn't want to select which features to control, these will be used.
-default_control_features = ["Young", "Smiling", "Male"]
+#default_control_features = ["Young", "Smiling", "Male"]
 
 
-if st.sidebar.checkbox("Show advanced options"):
+#if st.sidebar.checkbox("Show advanced options"):
     # Let the user pick which features to control with sliders.
-    control_features = st.sidebar.multiselect(
-        "Exclude which cells?",
-        ['Cell 1', 'Cell 2', 'Cell 3', 'Cell 4', 'Cell 5'],
-        ['Cell 1'],
-    )
-else:
-    # Don't let the user pick feature values to control.
-    control_features = default_control_features
+#    control_features = st.sidebar.multiselect(
+#        "Exclude which cells?",
+#        ['Cell 1', 'Cell 2', 'Cell 3', 'Cell 4', 'Cell 5'],
+#        ['Cell 1'],
+#    )
+#else:
+#    # Don't let the user pick feature values to control.
+#    control_features = default_control_features
 
 # Insert user-controlled values from sliders into the feature vector.
     
-st.sidebar.slider('Time', 0, 100, 50, 5)
+#st.sidebar.slider('Time', 0, 100, 50, 5)
 
 
 
-st.sidebar.title("Note")
-st.sidebar.write(
-    """The app is still in development.
-     """
-)
+#st.sidebar.title("Note")
+#st.sidebar.write(
+#    """The app is still in development.
+#     """
+#)
 
-st.sidebar.caption("Developed by: Christian Grech (DESY, MXL)")
-st.sidebar.caption(f"Streamlit version `{st.__version__}`")
+
+
+dataframe = pd.read_pickle('doocs_orbit_20221021_viewer.pkl')
+
 
 # Animation year by year basis
 
@@ -69,3 +68,6 @@ animation = px.scatter(data_frame=gapminder_df,
           size_max=100)
 
 st.plotly_chart(animation, use_container_width=True)
+
+st.caption("Developed by: Christian Grech (DESY, MXL)")
+st.caption(f"Streamlit version `{st.__version__}`")
